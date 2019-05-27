fb09a3168fdbe54351fbeebe12896e8bbf5f42e2
Fernando Lima <fernandovlima.s@gmail.com>
2019-05-27 20:07:56 -0300

    add actions to get all questions

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,5 +1,6 @@
 export const GET_QUESTION = "GET_QUESTION"
 export const ADD_QUESTION = "ADD_QUESTION"
+export const GET_ALL_QUESTIONS = "GET_ALL_QUESTIONS"
 
 export function getQuestion(question) {
   return {
@@ -12,5 +13,11 @@ export function addQuestion(question) {
   return {
     type: ADD_QUESTION,
     payload: question
+  }
+}
+
+export function getAllQuestions() {
+  return {
+    type: GET_ALL_QUESTIONS
   }
 }
