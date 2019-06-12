d0f97238df344ef693791c6f6fd00a03947a42db
Fernando Lima <fernandovlima.s@gmail.com>
2019-06-12 13:50:20 -0300

    api avatar

--- src/data/_DATA.js
+++ src/data/_DATA.js
@@ -2,7 +2,7 @@ let users = {
   sarahedo: {
     id: "sarahedo",
     name: "Sarah Edo",
-    avatarURL: "https://api.adorable.io/avatars/75/.png",
+    avatarURL: "https://robohash.org/sara",
     answers: {
       "8xf0y6ziyjabvozdd253nd": "optionOne",
       "6ni6ok3ym7mf1p33lnez": "optionTwo",
@@ -14,7 +14,7 @@ let users = {
   tylermcginnis: {
     id: "tylermcginnis",
     name: "Tyler McGinnis",
-    avatarURL: "https://api.adorable.io/avatars/75/.png",
+    avatarURL: "https://robohash.org/tyler",
     answers: {
       vthrdm985a262al8qx3do: "optionOne",
       xj352vofupe1dqz9emx13r: "optionTwo"
@@ -24,7 +24,7 @@ let users = {
   johndoe: {
     id: "johndoe",
     name: "John Doe",
-    avatarURL: "https://api.adorable.io/avatars/75/.png",
+    avatarURL: "https://robohash.org/john",
     answers: {
       xj352vofupe1dqz9emx13r: "optionOne",
       vthrdm985a262al8qx3do: "optionTwo",
