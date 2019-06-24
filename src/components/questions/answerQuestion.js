597cbde1fa2d58548a6a681d93fa6d5377c06d8d
Fernando Lima <fernandovlima.s@gmail.com>
2019-06-24 20:04:28 -0300

    answer question

--- src/components/questions/answerQuestion.js
+++ src/components/questions/answerQuestion.js
@@ -1,7 +1,29 @@
 import React from "react"
+import { connect } from "react-redux"
 
-const AnswerQuestion = props => {
-  console.log("props em question: ", props)
+const AnswerQuestion = ({ props, questions }) => {
+  const question_id = props.match.params.question_id
+  console.log("id da questao", question_id)
+  console.log("props", props)
+
+  const question = questions.filter(id => {
+    if (id === question_id) {
+      return question
+    }
+  })
+
+  console.log("====================================")
+  console.log(question)
+  console.log("====================================")
+
   return <div>vc esta em {props.match.params.question_id}</div>
 }
-export default AnswerQuestion
+
+const mapStateToProps = store => {
+  const { questions } = store
+
+  return {
+    questions
+  }
+}
+export default connect(mapStateToProps)(AnswerQuestion)
