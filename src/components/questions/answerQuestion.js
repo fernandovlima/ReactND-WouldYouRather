975fa494ce281610fc90d50e861615ab612dd6ac
Fernando Lima <fernandovlima.s@gmail.com>
2019-07-01 22:10:28 -0300

    adding question

--- src/components/questions/answerQuestion.js
+++ src/components/questions/answerQuestion.js
@@ -2,28 +2,25 @@ import React from "react"
 import { connect } from "react-redux"
 
 const AnswerQuestion = ({ props, questions }) => {
-  const question_id = props.match.params.question_id
-  console.log("id da questao", question_id)
+  //const question_id = props.match.params.question_id
+  //console.log("id da questao", question_id)
   console.log("props", props)
 
-  const question = questions.filter(id => {
-    if (id === question_id) {
-      return question
-    }
-  })
+  //   const question = questions.filter(id => {
+  //     if (id === question_id) {
+  //       return question
+  //     }
+  //   })
 
-  console.log("====================================")
-  console.log(question)
-  console.log("====================================")
-
-  return <div>vc esta em {props.match.params.question_id}</div>
+  return <div>vc esta em {props.question_id}</div>
 }
 
-const mapStateToProps = store => {
-  const { questions } = store
-
+const mapStateToProps = (store, props) => {
+  const questions = store
+  const id_question = props.match.params.question_id
   return {
-    questions
+    questions,
+    id_question
   }
 }
 export default connect(mapStateToProps)(AnswerQuestion)
--- src/components/questions/newQuestion.js
+++ src/components/questions/newQuestion.js
@@ -1,28 +1,65 @@
-import React from "react"
+import React, { Component } from "react"
 import { Form, Button } from "react-bootstrap"
+import { handleAddQuestion } from "../../actions/questions"
 
-const NewQuestion = () => {
-  return (
-    <div className="new-question-form">
-      <h2>Create the Question</h2>
-      <p>complete the question:</p>
-      <Form>
-        <Form.Group controlId="optionOne">
-          <Form.Control type="text" placeholder="enter optin one text here" />
-        </Form.Group>
-        <p>OR</p>
+class NewQuestion extends Component {
+  state = {
+    optionOne: "",
+    optionTwo: ""
+  }
 
-        <Form.Group controlId="optionTwo">
-          <Form.Control type="text" placeholder="enter optin two text here" />
-        </Form.Group>
+  handleChange = e => {
+    const option = e.target.value
 
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
-       
-    </div>
-  )
+    this.setState({
+      optionOne: option,
+      optionTwo: option
+    })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    const { optionOne, optionTwo } = this.state
+    const { dispatch, id } = this.props
+    dispatch(handleAddQuestion())
+    this.setState({
+      optionOne: "",
+      optionTwo: ""
+    })
+  }
+
+  render() {
+    return (
+      <div className="new-question-form">
+        <h2>Create the Question</h2>
+        <p>complete the question:</p>
+        <h5>Would you rather?</h5>
+        <Form onClick={this.handleSubmit}>
+          <Form.Group controlId="optionOne">
+            <Form.Control
+              type="text"
+              placeholder="enter optin one text here"
+              value={this.state.optionOne}
+            />
+          </Form.Group>
+          <p>OR</p>
+
+          <Form.Group controlId="optionTwo">
+            <Form.Control
+              type="text"
+              placeholder="enter optin two text here"
+              value={this.state.optionTwo}
+            />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+         
+      </div>
+    )
+  }
 }
 
 export default NewQuestion
