3761e7b722064695d3e0375d5645ae1607def87e
Fernando Lima <fernandovlima.s@gmail.com>
2019-07-03 12:47:44 -0300

    question answers

--- src/components/questions/answerQuestion.js
+++ src/components/questions/answerQuestion.js
@@ -3,28 +3,33 @@ import { connect } from "react-redux"
 import { getAllQuestionsAPI } from "../../actions/questions"
 
 class AnswerQuestion extends Component {
-  state = {}
-
   componentDidMount() {
     this.props.dispatch(getAllQuestionsAPI())
   }
   render() {
-    const question = this.state.questionOK
+    const question =
+      this.props.questionOK !== "undefined" ? this.props.questionOK : {}
     console.log("question ok em props", question)
+    console.log("Loading em props", this.props.loading)
+    const loading = this.props.loading
     return (
       <div>
-        {/* <div>
-          <div className="question-user">
-            <h5>{question.author} says...</h5>
+        {loading ? (
+          <div>
+            <div>
+              <div className="question-user">
+                <h5>{question.author} says...</h5>
+              </div>
+
+              <h5>Would you rather</h5>
+              <p>{question.optionOne}</p>
+              <p>OR</p>
+              <p>{question.optionTwo}</p>
+            </div>
           </div>
-          <div className="user-avatar">
-            <img src={question.id} alt="avatar" />
-          </div>
-          <h5>Would you rather</h5>
-          <p>{question.optionOne}</p>
-          <p>OR</p>
-          <p>{question.optionTwo}</p>
-        </div> */}
+        ) : (
+          <div>Error</div>
+        )}
       </div>
     )
   }
@@ -37,10 +42,13 @@ const mapStateToProps = (store, props) => {
   const id_question = props.match.params.question_id
   const questionOK = questions[id_question]
   console.log("question em mapstate", questionOK)
+
+  const loading = questionOK !== "undefined" ? true : false
   return {
     questions,
     id_question,
-    questionOK
+    questionOK,
+    loading
   }
 }
 export default connect(mapStateToProps)(AnswerQuestion)
