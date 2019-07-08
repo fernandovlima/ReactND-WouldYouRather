4570b637f69e6b8dcd34185fead6d5b94b51fbc9
Fernando Lima <fernandovlima.s@gmail.com>
2019-07-08 13:40:25 -0300

    handle radios

--- src/components/questions/answerQuestion.js
+++ src/components/questions/answerQuestion.js
@@ -4,9 +4,21 @@ import { getAllQuestionsAPI } from "../../actions/questions"
 import { Form, Button } from "react-bootstrap"
 
 class AnswerQuestion extends Component {
+  state = {
+    selectedOption: "question1"
+  }
   componentDidMount() {
     this.props.dispatch(getAllQuestionsAPI())
   }
+
+  handleOptionChange = e => {
+    this.setState({
+      selectedOption: e.target.value
+    })
+  }
+
+  handleSubmit = e => {}
+
   render() {
     const question =
       this.props.questionOK !== "undefined" ? this.props.questionOK : {}
@@ -27,20 +39,26 @@ class AnswerQuestion extends Component {
                 <Form.Check
                   type="radio"
                   label={question.optionOne.text}
+                  value="question1"
                   id="questionOne"
+                  checked={this.state.selectedOption === "question1"}
+                  onChange={this.handleOptionChange}
                 />
 
                 <Form.Check
                   type="radio"
+                  value="question2"
                   label={question.optionTwo.text}
                   id="questionTwo"
+                  checked={this.state.selectedOption === "question2"}
+                  onChange={this.handleOptionChange}
                 />
                 <Button type="submit">SUBMIT</Button>
               </Form>
             </div>
           </div>
         ) : (
-          <div>Error</div>
+          <div>Loading...</div>
         )}
       </div>
     )
