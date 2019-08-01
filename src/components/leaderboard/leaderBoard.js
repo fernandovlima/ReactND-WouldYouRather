09e835ee7868d384b2ec6a4dc80344e2d9ac784d
Fernando Lima <fernandovlima.s@gmail.com>
2019-08-01 14:27:30 -0300

    leaderboard layout

--- src/components/leaderboard/leaderBoard.js
+++ src/components/leaderboard/leaderBoard.js
@@ -1,26 +1,42 @@
 import React from "react"
 import { connect } from "react-redux"
 
-const LeaderBoard = props => {
+const LeaderBoard = ({ user }) => {
   // console.log("QUESTIONS:", this.props.questions)
-  // console.log("USER: ", this.props.user)
+  //console.log("USER: ", user)
   return (
     <div className="question-wraper">
       <div className="leaderboard-title">
         <h3>LeaderBoard</h3>
+        {user.map(user => (
+          <div className="leaderboard-list">
+            <div className="leaderboard-user">
+              <div className="user-avatar">
+                <img src={user.avatarURL} alt="avatar" />
+              </div>
+              <div classname="user-info">
+                <p>Ansered questions {user.answers.lenght}</p>
+                <p>Created questions {user.questions.lenght}</p>
+              </div>
+              <div className="user-total">TOTAL</div>
+            </div>
+          </div>
+        ))}
+        <div />
       </div>
     </div>
   )
 }
 
 const mapStateToProps = (store, props) => {
-  const { user, questions, autheduser } = store
-  console.log("USER", store)
-
+  const { user } = store
+  //const { answers } = user
+  console.log("USER", user)
+  const userAnsweredQuestions = Object.values(user).map(user => user.answers)
+  console.log("total answers:", userAnsweredQuestions)
   return {
     user,
-    questions,
-    autheduser
+    userAnsweredQuestions
   }
 }
 export default connect(mapStateToProps)(LeaderBoard)
