bd7e929dda6b00d9b28e0a0291ae89214e580864
Fernando Lima <fernandovlima.s@gmail.com>
2019-08-15 22:59:40 -0300

    layout on options

--- src/components/questions/answerQuestion.js
+++ src/components/questions/answerQuestion.js
@@ -1,33 +1,33 @@
-import React, { Component } from "react"
-import { connect } from "react-redux"
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import {
   getAllQuestionsAPI,
   handleSaveQuestionAnswer
-} from "../../actions/questions"
-import { Form, Button, ProgressBar } from "react-bootstrap"
+} from '../../actions/questions';
+import { Form, Button, ProgressBar, Row, Col } from 'react-bootstrap';
 
 class AnswerQuestion extends Component {
   state = {
-    selectedOption: "optionOne"
-  }
+    selectedOption: 'optionOne'
+  };
 
   componentDidMount() {
-    this.props.dispatch(getAllQuestionsAPI())
+    this.props.dispatch(getAllQuestionsAPI());
   }
 
   handleOptionChange = e => {
-    const value = e.target.value
-    this.setState({ selectedOption: value })
-  }
+    const value = e.target.value;
+    this.setState({ selectedOption: value });
+  };
   handleSubmit = e => {
-    e.preventDefault()
-    const { id_question, authedUser } = this.props
-    const answer = this.state.selectedOption
+    e.preventDefault();
+    const { id_question, authedUser } = this.props;
+    const answer = this.state.selectedOption;
 
     this.props.dispatch(
       handleSaveQuestionAnswer(authedUser, id_question, answer)
-    )
-  }
+    );
+  };
 
   render() {
     const {
@@ -37,59 +37,67 @@ class AnswerQuestion extends Component {
       voteTwo,
       voteTotal,
       loading
-    } = this.props
+    } = this.props;
 
     return (
       <div>
         {!loading ? (
-          <div className="container">
+          <div className='container'>
             <div>
-              <div className="question-user">
+              <div className='question-user'>
                 <h5>Asked by {questionOK.author} </h5>
               </div>
 
               <h5>Would you rather</h5>
+
               {isAnswered ? (
-                <div className="answered-question">
-                  <div className="answer-option">
+                <div className='answered-question'>
+                  <div className='answer-option'>
                     <span>Would you rather {questionOK.optionOne.text} ?</span>
                     <ProgressBar
                       now={(voteOne / voteTotal) * 100}
                       label={`${Math.floor((voteOne / voteTotal) * 100)}%`}
-                      variant={isAnswered ? "success" : "info"}
+                      variant={isAnswered ? 'success' : 'info'}
                     />
                     <p>{`${voteOne} of ${voteTotal} votes!`}</p>
                   </div>
-                  <div className="answer-option">
+                  <div className='answer-option'>
                     <span>Would you rather {questionOK.optionTwo.text} ? </span>
                     <ProgressBar
                       now={(voteTwo / voteTotal) * 100}
                       label={`${Math.floor((voteTwo / voteTotal) * 100)}%`}
-                      variant={isAnswered ? "success" : "info"}
+                      variant={isAnswered ? 'success' : 'info'}
                     />
                     <p>{`${voteTwo} of ${voteTotal} votes!`}</p>
                   </div>
                 </div>
               ) : (
                 <Form onSubmit={this.handleSubmit}>
-                  <Form.Check
-                    type="radio"
-                    label={questionOK.optionOne.text}
-                    value="optionOne"
-                    id="optionOne"
-                    checked={this.state.selectedOption === "optionOne"}
-                    onChange={this.handleOptionChange}
-                  />
-
-                  <Form.Check
-                    type="radio"
-                    value="optionTwo"
-                    label={questionOK.optionTwo.text}
-                    id="optionTwo"
-                    checked={this.state.selectedOption === "optionTwo"}
-                    onChange={this.handleOptionChange}
-                  />
-                  <Button type="submit" onClick={this.handleSubmit}>
+                  <Row className='justify-content-md-center'>
+                    <Col md='auto'>
+                      <Form.Check
+                        type='radio'
+                        label={questionOK.optionOne.text}
+                        value='optionOne'
+                        id='optionOne'
+                        checked={this.state.selectedOption === 'optionOne'}
+                        onChange={this.handleOptionChange}
+                      />
+                    </Col>
+                  </Row>
+                  <Row className='justify-content-md-center'>
+                    <Col md='auto'>
+                      <Form.Check
+                        type='radio'
+                        value='optionTwo'
+                        label={questionOK.optionTwo.text}
+                        id='optionTwo'
+                        checked={this.state.selectedOption === 'optionTwo'}
+                        onChange={this.handleOptionChange}
+                      />
+                    </Col>
+                  </Row>
+                  <Button type='submit' onClick={this.handleSubmit}>
                     SUBMIT
                   </Button>
                 </Form>
@@ -100,27 +108,27 @@ class AnswerQuestion extends Component {
           <div>Loading...</div>
         )}
       </div>
-    )
+    );
   }
 }
 
 const mapStateToProps = (store, props) => {
-  const { questions, user } = store
-  const { authedUser } = user
+  const { questions, user } = store;
+  const { authedUser } = user;
 
-  const id_question = props.match.params.question_id
-  const questionOK = questions[id_question]
+  const id_question = props.match.params.question_id;
+  const questionOK = questions[id_question];
 
-  const { answers } = typeof user !== "undefined" ? user[authedUser] : {}
-  const loading = typeof questionOK === "undefined" ? true : false
+  const { answers } = typeof user !== 'undefined' ? user[authedUser] : {};
+  const loading = typeof questionOK === 'undefined' ? true : false;
 
   let voteOne,
     voteTwo,
-    voteTotal = 0
+    voteTotal = 0;
   if (!loading) {
-    voteOne = questionOK.optionOne.votes.length
-    voteTwo = questionOK.optionTwo.votes.length
-    voteTotal = voteOne + voteTwo
+    voteOne = questionOK.optionOne.votes.length;
+    voteTwo = questionOK.optionTwo.votes.length;
+    voteTotal = voteOne + voteTwo;
   }
 
   return {
@@ -131,8 +139,8 @@ const mapStateToProps = (store, props) => {
     voteOne,
     voteTwo,
     voteTotal,
-    isAnswered: typeof answers[id_question] !== "undefined" ? true : false,
+    isAnswered: typeof answers[id_question] !== 'undefined' ? true : false,
     loading
-  }
-}
-export default connect(mapStateToProps)(AnswerQuestion)
+  };
+};
+export default connect(mapStateToProps)(AnswerQuestion);
