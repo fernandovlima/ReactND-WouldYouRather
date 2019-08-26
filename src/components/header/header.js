30eb6353df84f5dc96eb4606e536a967636396cf
Fernando Lima <fernandovlima.s@gmail.com>
2019-08-25 21:08:57 -0300

    header style add

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -25,11 +25,16 @@ const Header = ({ authedUser, dispatch, history }) => {
               LEADERBOARD
             </NavLink>
             {authedUser && (
-              <div className='nav-link user-logged '>Hello {authedUser}</div>
+              <div>
+                <div className='nav-link user-logged '>
+                  Hello {authedUser}
+                  <Navbar.Brand className='nav-link header-link ' href='/'>
+                    LOGOUT
+                  </Navbar.Brand>
+                </div>
+              </div>
             )}
-            <Navbar.Brand className='nav-link' href='/'>
-              LOGOUT
-            </Navbar.Brand>
+
             {/* <NavLink to='/' className='nav-link'>
               LOGOUT
             </NavLink> */}
